71cf293b959ec12ca0f4b2e3d5201d64bd1cd928
kyubek <69163151+orionkumikuze@users.noreply.github.com>
2023-06-21 21:11:13 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from engine.engine import *
+from .engine import *
 import sys
 import pygame
-from engine.controller import *
+from .controller import *
 screen = pygame.display.set_mode((600,800))
 wait = 0
 waitTime = 0
